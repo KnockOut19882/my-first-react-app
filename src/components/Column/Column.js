74d20702c52b3b46686d5a0f2051eeb39981731f
Mateusz <mpodolski1988@gmail.com>
2025-09-18 21:22:34 +0200

    Change: Column, ColumnForm, CardForm and store.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -12,7 +12,7 @@ return (
             <ul className={styles.cards}>
                 {cards.map(card => <Card key={card.id}{...card} title={card.title} />)}
             </ul>
-            <CardForm columnId={props.id} action={props.action} />
+            <CardForm columnId={props.id} />
         </article>
     </>
 );
