731aab0ddd8f27149705a5a8c162806906bccc1d
Mateusz <mpodolski1988@gmail.com>
2025-09-19 17:21:24 +0200

    Change: Favorites, About App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import Hero from './components/Hero/Hero';
-import SearchForm from './components/SearchForm/SearchForm';
-import List from './components/List/List';
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
+import { Routes, Route } from 'react-router-dom';
+import About from './components/About/About';
+import Favorites from './components/Favorites/Favorites';
 
 const App = () => {
   return (
-    <>
+    <main>
       <NavBar />
       <Container>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
       </Container>
-    </>
+ </main>
   );
 };
 
---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,10 @@
+import styles from './Favorites.module.scss';
+
+const Favorites = () => (
+  <div className={styles['favorites']}>
+    <h1>Favorites</h1>
+    <p>This is the favorites page of our application.</p>
+  </div>
+);
+
+export default Favorites;
